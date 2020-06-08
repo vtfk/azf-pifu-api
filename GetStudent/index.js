7ed518baaa552f0919a48531dec32dc6b8cba763
Mats Andreassen <matsand@live.no>
2020-06-09 00:24:08 +0200

    Implemented /student/:id

---
+++ GetStudent/index.js
@@ -0,0 +1,71 @@
+const withTokenAuth = require('../lib/token-auth')
+const { getStudent } = require('../lib/api/students')
+const { getTeacher } = require('../lib/api/teachers')
+const repackStudent = require('../lib/repack-student')
+
+const returnStudents = async function (context, req) {
+  const { caller } = req.token
+  const { id } = context.bindingData
+
+  // Verify that we got a caller!
+  if(!caller) {
+    context.res = {
+      status: 403,
+      body: `Couldn't read caller from token!`
+    }
+    return
+  }
+
+  try {
+    // Get teacher from caller
+    const teacher = await getTeacher(context, caller)
+    if(!teacher) {
+      context.log.warn(['pifu-api', 'student', caller, 'get student', id, 'teacher not found'])
+      context.res = {
+        status: 403,
+        body: `Teacher not found: ${caller}`
+      }
+      return
+    }
+    
+    // Get students matching the provided username
+    const student = await getStudent(context, id)
+    if(!student) {
+      context.log.warn(['pifu-api', 'student', caller, 'get student', id, 'student not found', id])
+      context.res = {
+        status: 404,
+        body: `Student not found: ${id}`
+      }
+      return
+    }
+
+    // Get intersecting groups between student and teacher
+    const studentGroupIds = student.groupIds || []
+    const teacherGroupIds = teacher.groupIds || []
+    const intersection = [...teacherGroupIds].filter(groupId => studentGroupIds.includes(groupId))
+
+    // No, this aint my student!
+    if(intersection.length === 0) {
+      context.log(['pifu-api', 'student', caller, 'get student', id, 'student not related to teacher'])
+      context.res = {
+        status: 401,
+        body: `Access denied to student: ${id}`
+      }
+      return
+    }
+
+    context.log(['pifu-api', 'student', caller, 'get student', student.username])
+    const repackedStudent = repackStudent(context, student, teacher)
+    context.res = {
+      body: [repackedStudent]
+    }
+  } catch (error) {
+    context.log.error(['pifu-api', 'student', caller, 'get student', 'error', error.message])
+    context.res = {
+      status: 500,
+      body: error.message
+    }
+  }
+}
+
+module.exports = (context, request) => withTokenAuth(context, request, returnStudents)
