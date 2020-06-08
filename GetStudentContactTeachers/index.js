1511d586559edf66e59b21b4a83722405081aab3
Mats Andreassen <matsand@live.no>
2020-06-09 00:24:38 +0200

    Implemented method to get students contact teacher

---
+++ GetStudentContactTeachers/index.js
@@ -0,0 +1,54 @@
+const withTokenAuth = require('../lib/token-auth')
+const { getStudent } = require('../lib/api/students')
+const { getTeachers } = require('../lib/api/teachers')
+const repackTeacher = require('../lib/repack-teacher')
+
+const returnStudents = async function (context, req) {
+  const { caller } = req.token
+  const { id } = context.bindingData
+
+  // Verify that we got a caller!
+  if(!caller) {
+    context.res = {
+      status: 403,
+      body: `Couldn't read caller from token!`
+    }
+    return
+  }
+
+  try {
+    // Get students matching the provided username
+    const student = await getStudent(context, id)
+    if(!student) {
+      context.log.warn(['pifu-api', 'student', caller, 'get contactteachers', 'student not found', id])
+      context.res = {
+        status: 404,
+        body: `Student not found: ${id}`
+      }
+      return
+    }
+    
+    const { kontaktlarergruppeIds, ordenIds, atferdIds } = student
+    const kontaktIds = [...kontaktlarergruppeIds, ...ordenIds, ...atferdIds]
+
+    // Get teachers matching the contact class ids
+    const teachers = await getTeachers(context, {
+      groupIds: { $in: kontaktIds }
+    })
+
+
+    context.log(['pifu-api', 'students', caller, 'get contactteachers', student.username])
+    const repackedTeachers = teachers.map((teacher) => repackTeacher(context, teacher))
+    context.res = {
+      body: repackedTeachers
+    }
+  } catch (error) {
+    context.log.error(['pifu-api', 'students', caller, 'get contactteachers', 'error', error.message])
+    context.res = {
+      status: 500,
+      body: error.message
+    }
+  }
+}
+
+module.exports = (context, request) => withTokenAuth(context, request, returnStudents)
