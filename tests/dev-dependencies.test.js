ccdc5c715534dd1ed185c82559eabb6f330396f5
Mats Andreassen <matsand@live.no>
2020-06-09 00:47:30 +0200

    Added tests

---
+++ tests/dev-dependencies.test.js
@@ -0,0 +1,17 @@
+const test = require('ava')
+const { devDependencies } = require('../package.json')
+const dropModules = []
+const isDropped = (module) => !dropModules.includes(module)
+
+if (Object.keys(devDependencies).length > 0) {
+  Object.keys(devDependencies).filter(isDropped).forEach((dependency) => {
+    test(`${dependency} loads ok`, t => {
+      const module = require(dependency)
+      t.truthy(module)
+    })
+  })
+} else {
+  test('no dev-dependecies to test', t => {
+    t.truthy(true)
+  })
+}
