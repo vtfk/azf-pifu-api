29365e7aafd5315d7f90ef1df2d3d1fd27f85ca1
Mats Andreassen <matsand@live.no>
2020-06-19 22:04:48 +0200

    Verify that the teacher have access to the student before returning any data

--- GetClassStudents/index.js
+++ GetClassStudents/index.js
@@ -2,17 +2,38 @@ const withTokenAuth = require('../lib/token-auth')
 const { getClass } = require('../lib/api/classes')
 const { getStudents } = require('../lib/api/students')
 const repackStudent = require('../lib/repack-student')
+const { getTeacher } = require('../lib/api/teachers')
 
 const returnClasses = async function (context, req) {
   const caller = req.token.caller
   const { id } = context.bindingData
 
+  if (!caller) {
+    context.res = {
+      status: 401,
+      body: 'Couldn\'t read caller from token!'
+    }
+    return
+  }
+
   try {
+    // Get teacher
+    const teacher = await getTeacher(context, caller)
+    if (!teacher) {
+      context.log.warn(['pifu-api', 'classes', caller, 'students', id, 'teacher not found'])
+      context.res = {
+        status: 401,
+        body: `Teacher not found: ${caller}`
+      }
+      return
+    }
+
     const classes = await getClass(context, id)
     if (!classes) {
+      context.log.warn(['pifu-api', 'classes', caller, 'students', id, 'class not found'])
       context.res = {
         status: 403,
-        body: `Class not found: ${id}`
+        body: []
       }
       return
     }
@@ -20,7 +41,19 @@ const returnClasses = async function (context, req) {
     const students = await getStudents(context, { groupIds: classes.id })
     context.log.info(['pifu-api', 'class', id, 'students', caller, 'length', students.length])
 
-    const repackedStudents = students.map((student) => repackStudent(context, student))
+    // Get students the teacher have relation
+    const teacherStudents = students.filter(student => student.groupIds && student.groupIds.includes(classes.id) && student.groupIds.some(groupId => teacher.groupIds.includes(groupId)))
+
+    if (teacherStudents.length === 0 && !teacher.groupIds.includes(classes.id)) {
+      context.log.warn(['pifu-api', 'classes', caller, 'students', id, 'teacher not related to group'])
+      context.res = {
+        status: 403,
+        body: []
+      }
+      return
+    }
+
+    const repackedStudents = students.map((student) => repackStudent(context, student, teacher))
     context.res = {
       body: repackedStudents
     }
