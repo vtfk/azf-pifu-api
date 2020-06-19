d6013cb6a2289fc9f7c3d938e57d42466790e3d2
Mats Andreassen <matsand@live.no>
2020-06-19 22:04:06 +0200

    Verify that the current caller have access to the class to get a list of the members ssn

--- GetClass/index.js
+++ GetClass/index.js
@@ -1,16 +1,48 @@
 const withTokenAuth = require('../lib/token-auth')
 const { getClass } = require('../lib/api/classes')
+const { getTeacher } = require('../lib/api/teachers')
+const repackClasses = require('../lib/repack-class-members')
 
 const returnClass = async function (context, req) {
   const caller = req.token.caller
   const { id } = context.bindingData
 
+  if (!caller) {
+    context.res = {
+      status: 401,
+      body: 'Couldn\'t read caller from token!'
+    }
+    return
+  }
+
   try {
+    // Get teacher
+    const teacher = await getTeacher(context, caller)
+    if (!teacher) {
+      context.log.warn(['pifu-api', 'classes', caller, 'get class', id, 'teacher not found'])
+      context.res = {
+        status: 401,
+        body: `Teacher not found: ${caller}`
+      }
+      return
+    }
+
     const classes = await getClass(context, id)
     if (!classes) {
+      context.log.warn(['pifu-api', 'classes', caller, 'get class', id, 'class not found'])
       context.res = {
         status: 403,
-        body: `Class not found: ${id}`
+        body: []
+      }
+      return
+    }
+
+    if (!teacher.groupIds.includes(classes.id)) {
+      context.log(['pifu-api', 'classes', caller, 'get class', id, 'teacher not related to group, but returning groups with hidden members'])
+
+      const repackedClass = repackClasses(classes)
+      context.res = {
+        body: [repackedClass]
       }
       return
     }
